b123ec7e2e48869d3ac588f499a50202c50d7e70
ngDmanh <manh024011@gmail.com>
2024-04-21 00:27:29 +0700

    tạo gợi ý tìm kiếm từ bằng adapter

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,17 +12,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //Khai báo biến
-    EditText edt1;
-    Button btn1, btn2;
+    // Khai báo biến
+    AutoCompleteTextView autoCompleteTextView;
+    Button btn1;
+
+    // Danh sách từ gợi ý
+    String[] suggestedWords = {"Apple", "Agree", "Banana","Boring", "Orange", "Pineapple", "Mango", "Grapes"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        edt1 = findViewById(R.id.editTextText);
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
         btn1 = findViewById(R.id.button);
-        btn2 = findViewById(R.id.button2);
+
+
+        // Thiết lập Adapter cho AutoCompleteTextView
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, suggestedWords);
+        autoCompleteTextView.setAdapter(adapter);
+        autoCompleteTextView.setThreshold(1); // Khắc phục lỗi ko hiện gợi ý
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
